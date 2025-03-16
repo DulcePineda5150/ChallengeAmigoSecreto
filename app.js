eeeb04466c4b94fbb41f613ade4feba37246beec
Dulce Pineda <dulceitzel_5150@hotmail.com>
2025-03-16 02:50:28 -0600

    Se agrega funcion Recorrer arreglo

--- app.js
+++ app.js
@@ -11,6 +11,19 @@ function agregarAmigo() {
         arrayNombres.push(nombre);
         alert("se ha agregado tu nombre correctamente");
         document.getElementById("amigo").value= "";
+        RecorrerArreglo();
+        
     }
     else {alert("Por favor, ingrese un nombre")};
+}
+
+function RecorrerArreglo () {
+
+    let listanombre = document.getElementById("listaAmigos");
+    let nombres;
+    listanombre.innerHTML = "";
+    for (let i=0;i<arrayNombres.length; i++){
+        listanombre.innerHTML+= `<li>${arrayNombres[i]}</li>`;
+    }
+    
 }
