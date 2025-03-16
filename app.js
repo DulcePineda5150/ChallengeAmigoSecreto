87494b6282d01be2f197a476b4a29ae6d5ced0b1
Dulce Pineda <dulceitzel_5150@hotmail.com>
2025-03-16 01:40:21 -0600

    Se agrega función Agregar

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let arrayNombres= [];
+
+
+function agregarAmigo() {
+    /*alert ('Click en botón');*/
+    let nombre = document.getElementById("amigo").value;
+    /*alert (nombre);*/
+    if (nombre!=""){
+        arrayNombres.push(nombre);
+        alert("se ha agregado tu nombre correctamente");
+        document.getElementById("amigo").value= "";
+    }
+    else {alert("Por favor, ingrese un nombre")};
+}
